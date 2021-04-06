4dbf3ed0771c243cfee218a1be43785037a4f385
xyzjb <57247378+xyzjb@users.noreply.github.com>
2021-04-06 15:28:36 +0800

    Create testpvc1.py

---
+++ kmmlds/testpvc1.py
@@ -0,0 +1,22 @@
+import argparse
+import sys
+from pathlib import Path
+
+
+def main(args):
+    parser = argparse.ArgumentParser(description='Returns sum of two arguments')
+    parser.add_argument("--file1", type=str, required=True)
+    #parser.add_argument("--param2", type=str, required=True)
+
+    args = parser.parse_args(args)
+
+    Path('/data/1/file1.txt').parent.mkdir(parents=True, exist_ok=True)
+    #path=Path('/data/1')
+    #path.mkdir()
+    with open('/data/1/file1.txt', 'w') as sum_path:
+        sum_path.write(args.file1)
+    
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
